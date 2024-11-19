4d2ace900ec9cd08a85b99379bd67d090afae4ee
Your Name <you@example.com>
2024-11-19 12:31:52 +0530

    sep unit place

--- test.c
+++ test.c
@@ -1,4 +1,23 @@
+#include<stdio.h>
 int main()
 {
-	printf("disha gangurde")
+int x;
+printf("\n enter value of x");
+scanf("%d",&x);
+if (x>0)
+{
+printf("\n number is positive");
 }
+else
+{
+printf("\n number is negative");
+}
+int y=(x%10);
+{
+printf("unit place is %d",y);
+}  
+return 0;
+}
+
+
+
